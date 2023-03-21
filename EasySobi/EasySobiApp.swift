79aa2dbbdf750b0a3ecff1c960a2946b13419015
yujinkim1 <yujin7621@me.com>
2023-03-21 21:18:51 +0900

    add: Pure project files

---
+++ EasySobi/EasySobiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasySobiApp.swift
+//  EasySobi
+//
+//  Created by Yujin Kim on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct EasySobiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
