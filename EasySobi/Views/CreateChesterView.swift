3debae2c67035a9da0df8511bc8d973cd90c2f20
yujinkim1 <yujin7621@me.com>
2023-04-07 23:28:20 +0900

    add: Create chester view

---
+++ EasySobi/Views/CreateChesterView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateChesterView.swift
+//  EasySobi
+//
+//  Created by Yujin Kim on 2023-04-07.
+//
+
+import SwiftUI
+
+struct CreateChesterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateChesterView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateChesterView()
+    }
+}
