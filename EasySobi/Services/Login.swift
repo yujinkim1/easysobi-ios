2160bb753b5f3fe955cb9debfb2a7b44bdc8a43c
yujinkim1 <yujin7621@me.com>
2023-04-09 23:47:14 +0900

    modified: Bundle up observable object

--- EasySobi/Services/Login.swift
+++ EasySobi/Services/Login.swift
@@ -10,69 +10,91 @@ import Alamofire
 import KakaoSDKAuth
 import KakaoSDKUser
 
-
-let restApiKey = "6fdc687c6c1fa1c6eac26a2db1889ae4"
-
-let accessToken = "kakao_access_token"
-
-@MainActor
-func kakaoLogin() {
-    Task {
-        if UserApi.isKakaoTalkLoginAvailable() {
-            await loginWithKakaoTalkAppHandler()
-        } else {
-            await loginWithKakaoAccount()
-        }
-    }
-}
-
-func loginWithKakaoTalkAppHandler() async -> Bool {
-    await withCheckedContinuation {
-        continuation in
-        UserApi.shared.loginWithKakaoTalk {
-            (OAuthToken, error) in
-            if let error = error {
-                print(error)
-                continuation.resume(returning: false)
+class Login: ObservableObject {
+    //let restApiKey = "6fdc687c6c1fa1c6eac26a2db1889ae4"
+    @Published var currentUser: User?
+    @Published var accessToken: OAuthToken?
+    
+    @MainActor
+    func kakaoLogin() {
+        Task {
+            if UserApi.isKakaoTalkLoginAvailable() {
+                await loginWithKakaoTalkAppHandler()
             } else {
-                print("loginWithKakaoTalk() is success.")
-                let token = OAuthToken
+                await loginWithKakaoAccountHandler()
             }
         }
     }
-}
-
-func loginWithKakaoAccountHandler() async -> Bool {
-    await withCheckedContinuation {
-        continuation in
-        UserApi.shared.loginWithKakaoAccount{
-            (OAuthToken, error) in
-            if 
-        }
-    }
-}
-
-//MARK: Kakao Logout
-@MainActor
-func kakaoLogout() {
-    Task {
-        if await kakaoLogoutHandler() {
-            try? Auth.shared.tokenManager.deleteToken()
-            //MARK : 사용자 정보 삭제해야함, currentUser = nil
-        }
-    }
-}
-
-func kakaoLogoutHandler() async -> Bool {
-    await withCheckedContinuation {
-        continuation in UserApi.shared.logout {
-            (error) in
+    
+    func getUserData() {
+        UserApi.shared.me() {(user, error) in
             if let error = error {
-                print(error)
-                continuation.resume(returning: false)
-            } else {
-                print("kakaoLogout() success.")
-                continuation.resume(returning: true)
+                print(error.localizedDescription)
+            }
+            else {
+                // kakaoUserData = user
+                // isLogined = true
+            }
+        }
+    }
+
+    func loginWithKakaoTalkAppHandler() async -> Bool {
+        await withCheckedContinuation {
+            continuation in
+            UserApi.shared.loginWithKakaoTalk {
+                (OAuthToken, error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                } else {
+                    print("loginWithKakaoTalk() is success.")
+                    _ = OAuthToken
+                    // 로그인 성공 시 currentUser와 accessToken 프로퍼티에 값을 할당
+                    // currentUser = User
+                    // accessToken = token
+                    continuation.resume(returning: true)
+                }
+            }
+        }
+    }
+
+    func loginWithKakaoAccountHandler() async -> Bool {
+        await withCheckedContinuation {
+            continuation in
+            UserApi.shared.loginWithKakaoAccount{
+                (OAuthToken, error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                } else {
+                    print("loginWithKakaoAccountHandler() is success.")
+                    let token = OAuthToken
+                }
+            }
+        }
+    }
+
+    //MARK: Kakao Logout
+    @MainActor
+    func kakaoLogout() {
+        Task {
+            if await kakaoLogoutHandler() {
+                //MARK : 사용자 정보 삭제해야함, try? AuthApi, currentUser = nil
+            }
+        }
+    }
+
+    func kakaoLogoutHandler() async -> Bool {
+        await withCheckedContinuation {
+            continuation in UserApi.shared.logout {
+                (error) in
+                if let error = error {
+                    print(error)
+                    continuation.resume(returning: false)
+                } else {
+                    print("kakaoLogout() success.")
+                    continuation.resume(returning: true)
+                }
             }
         }
     }
